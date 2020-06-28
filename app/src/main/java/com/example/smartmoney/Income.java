e04e770e00bbb661b39864f3dd7d647f7f6e9aa5
irdina97 <56860009+irdina97@users.noreply.github.com>
2020-06-28 14:46:02 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/smartmoney/Income.java
@@ -0,0 +1,23 @@
+package com.example.smartmoney;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Income extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_income);
+
+
+        // EditText editText = findViewById(R.id.editText);
+        // MyKeyboard keyboard = findViewById(R.id.keyboard);
+        // editText.setRawInputType(InputType.TYPE_CLASS_TEXT);
+        // editText.setTextIsSelectable(true);
+
+        //InputConnection ic = editText.onCreateInputConnection(new EditorInfo());
+        // keyboard.setInputConnection(ic);
+    }
+}
--- app/src/main/java/com/example/smartmoney/MainActivity.java
+++ app/src/main/java/com/example/smartmoney/MainActivity.java
@@ -1,14 +1,138 @@
 package com.example.smartmoney;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.cardview.widget.CardView;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
+
+import com.google.android.material.navigation.NavigationView;
+
+
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private DrawerLayout drawer;
+    private Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn = findViewById(R.id.btn);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, Calendar.class);
+                startActivity(intent);
+            }
+        });
+
+        CardView cardView = findViewById(R.id.card_view);
+        CardView cardView1 = findViewById(R.id.card_view1);
+        CardView cardView2 = findViewById(R.id.card_view2);
+
+        cardView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Expenses.class);
+                startActivity(intent);
+            }
+        });
+        cardView1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Expenses.class);
+                startActivity(intent);
+            }
+        });
+        cardView2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(MainActivity.this, Income.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //Toolbar
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        drawer = findViewById(R.id.drawer_layout);
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar,
+                R.string.navigation_draw_open, R.string.navigation_draw_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    new ChartFragment()).commit();
+            navigationView.setCheckedItem(R.id.nav_piechart);
+        }
+
+
     }
+
+   /* public void openCalendar(){
+        Intent intent = new Intent(MainActivity.this, Income.class);
+        startActivity(intent);
+    }*/
+
+    //Navigation menu
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.nav_piechart:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new ChartFragment()).commit();
+                break;
+            case R.id.nav_category:
+                getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                        new CategoryFragment()).commit();
+                break;
+            case R.id.nav_settings:
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_rate:
+                Toast.makeText(this, "Rate", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.nav_info:
+                Toast.makeText(this, "Info", Toast.LENGTH_SHORT).show();
+                break;
+
+        }
+
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+
+    }
+
+
+
 }
+
