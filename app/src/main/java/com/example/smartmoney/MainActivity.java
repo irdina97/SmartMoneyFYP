40d0dab26e63605031744206ef3c80aebd62056e
irdina97 <irdina.azhar97@gmail.com>
2020-06-30 22:54:39 +0800

    commit calc

--- app/src/main/java/com/example/smartmoney/MainActivity.java
+++ app/src/main/java/com/example/smartmoney/MainActivity.java
@@ -48,7 +48,7 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             @Override
             public void onClick(View v) {
 
-                Intent intent = new Intent(MainActivity.this, Expenses.class);
+                Intent intent = new Intent(MainActivity.this, Income.class);
                 startActivity(intent);
             }
         });
